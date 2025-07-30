e8485c24d4504ee11816b1e5aa3ec579506b2cec
零Rei <othiym23@gmail.com>
2025-07-30 16:03:31 -0700

    updating files before system software update

--- dot_config/private_fish/functions/upgrade-everything.fish
+++ dot_config/private_fish/functions/upgrade-everything.fish
@@ -1,16 +1,19 @@
 function upgrade-everything --description 'Run all the update commands that can be run without intervention'
-date
-and brew upgrade
-and mas upgrade
-and nvim --headless "+Lazy! sync" +qa
-and rustup update
-and asdf latest --all
-if begin chezmoi verify ; end
-	echo "No changes to tracked dotfiles."
-else
-	echo "Chezmoi status:"
-	chezmoi diff --reverse
-	chezmoi status
-end
-date
+    date
+    nvim --headless "+Lazy! sync" +qa
+    and brew upgrade
+    and mas upgrade
+    and rustup update
+    and asdf latest --all
+    and softwareupdate -ia
+    and if begin
+            chezmoi verify
+        end
+        echo "No changes to tracked dotfiles."
+    else
+        echo "Chezmoi status:"
+        chezmoi diff --reverse
+        chezmoi status
+    end
+    date
 end
